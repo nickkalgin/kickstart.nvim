27eebdf19663569b0a79061e7231659515c9a534
Nick Kalgin <1064416+nickkalgin@users.noreply.github.com>
2026-01-07 00:29:14 +0200

    Ignore compiled Java

--- init.lua
+++ init.lua
@@ -300,11 +300,18 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          file_ignore_patterns = {
+            'target/',
+            'build/',
+            '.gradle/',
+            '.idea/',
+            '%.class',
+          },
+          -- mappings = {
+          --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          -- },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
