1f250b38223f57acaab02ca0e112791d371f6dac
Nick Kalgin <1064416+nickkalgin@users.noreply.github.com>
2026-02-17 22:10:53 +0200

    Lua LSP config recognizes 'vim' variable

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -21,6 +21,17 @@ return {
       completion = {
         callSnippet = 'Replace',
       },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require',
+        },
+      },
       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
       -- diagnostics = { disable = { 'missing-fields' } },
     },
